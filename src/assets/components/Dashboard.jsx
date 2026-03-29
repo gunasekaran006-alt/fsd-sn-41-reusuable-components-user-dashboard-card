75f20fdfbd6b228b338e9ac5b68c0e724496122c
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-03-29 14:26:33 +0530

    updates

--- src/assets/components/Dashboard.jsx
+++ src/assets/components/Dashboard.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import Button from './Button'
 
-function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+function Dashboard(props) {
+    return (
+        <div>
+            {
+                props.data.loginstatus ? (
+                    <div>
+                        <h2>Welcome,{props.data.name}</h2>
+                        <p>{props.data.role}</p>
+
+
+                        {
+                            props.data.role === "developer" ? (
+                                <Button name="Submit Code" />
+                            ) : props.data.role === "lead" ? (
+                                <div>
+                                    <Button name="Create Task" />
+                                    <Button name="Approve Task" />
+                                </div>
+                            ) : props.data.role === "manager" ? (
+                                <div>
+                                    <Button name="Create Task" />
+                                    <Button name="Delete Task" />
+                                    <Button name="Approve Task" />
+                                </div>
+                            ) : (
+                                <p>Access Denied</p>
+                            )
+                        }
+                    </div>
+                ) : (
+                    <p>Please Login First</p>
+                )
+            }
+        </div>
+    )
 }
 
 export default Dashboard
